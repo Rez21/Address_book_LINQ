dcdf75e1496f080cd1004299c3bbeb61db72bcc4
Prasad <banprasad21@gmail.com>
2022-10-10 02:39:35 +0530

    [Raghav] Add: UC2 adding attributes to table

--- AddressBookLinq/AddressBookTable.cs
+++ AddressBookLinq/AddressBookTable.cs
@@ -1,12 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AddressBookLinq
 {
-    internal class AddressBookTable
+    public class AddressBookTable
     {
+        // UC 1 Create a new DataTable
+        DataTable table = new DataTable("AddressBook");
+
+
+        // UC 2 Initializes a new instance of the class.
+
+        public AddressBookTable()
+        {
+            table.Columns.Add("FirstName", typeof(string));
+            table.Columns.Add("LastName", typeof(string));
+            table.Columns.Add("Address", typeof(string));
+            table.Columns.Add("City", typeof(string));
+            table.Columns.Add("State", typeof(string));
+            table.Columns.Add("Zip", typeof(string));
+            table.Columns.Add("PhoneNumber", typeof(string));
+            table.Columns.Add("Email", typeof(string));
+        }
     }
 }
