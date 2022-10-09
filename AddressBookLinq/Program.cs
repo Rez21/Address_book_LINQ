0b6a1e3cb8e5de89830f62d500619d3f710c28cd
Prasad <banprasad21@gmail.com>
2022-10-10 02:31:55 +0530

    [Raghav] Add: UC1 - creating datatable

---
+++ AddressBookLinq/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AddressBookLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // UC 1 Create a new table
+            AddressBookTable addressBookTable = new AddressBookTable();
+        }
+    }
+}
