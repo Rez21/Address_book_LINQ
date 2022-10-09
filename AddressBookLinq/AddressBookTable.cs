bde02265d02053eabdfd50122c600598fa24d026
Prasad <banprasad21@gmail.com>
2022-10-10 02:41:41 +0530

    [Raghav] Add: UC3 inserting data into table

--- AddressBookLinq/AddressBookTable.cs
+++ AddressBookLinq/AddressBookTable.cs
@@ -25,6 +25,15 @@ namespace AddressBookLinq
             table.Columns.Add("Zip", typeof(string));
             table.Columns.Add("PhoneNumber", typeof(string));
             table.Columns.Add("Email", typeof(string));
+
+            // UC3 - add data to table
+            table.Rows.Add("Raghav", "Ban", "Center Mall", "Yavatmal", "Maharashtra", "445001", "9595959595", "rban@gmail.com");
+            table.Rows.Add("Ajay", "Rathod", "Ner", "Yavatmal", "Maharashtra", "125121", "8570934858", "aj@gmail.com");
+            table.Rows.Add("Akshay", "Poriya", "Goregaon", "Bangalore", "Karnataka", "125433", "8585858585", "akshay.a@gmail.com");
+            table.Rows.Add("Kretika", "Arora", "Andheri", "Mumbai", "Maharashtra", "125445", "7575757575", "kretika.arora@gmail.com");
+            table.Rows.Add("Praveen", "Kumar", "rohini", "Delhi", "Delhi", "435121", "7897897898", "praveen.kumar@gmail.com");
+            table.Rows.Add("Apoorva", "Singh", "Andheri", "Mumbai", "Maharashtra", "125445", "8598598599", "apporva.singh@gmail.com");
         }
+    }
     }
 }
